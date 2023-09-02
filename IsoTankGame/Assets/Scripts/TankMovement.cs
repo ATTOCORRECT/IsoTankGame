ff750d51276c9fda34d2e0f956424d30c0128496
ATTOCORRECT <97545662+ATTOCORRECT@users.noreply.github.com>
2023-09-01 21:46:44 -0400

    Added 'Dime Turning'
    you can now turn on the spot!

--- IsoTankGame/Assets/Scripts/TankMovement.cs
+++ IsoTankGame/Assets/Scripts/TankMovement.cs
@@ -19,15 +19,38 @@ public class TankMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        lVelocity = Input.GetAxis("LeftThrottle") * movementSpeed;
-        rVelocity = Input.GetAxis("RightThrottle") * movementSpeed;
+        lVelocity *= 0.8f;
+        rVelocity *= 0.8f;
 
-        if (lVelocity != 0) {
+        
+
+        float lInput = Input.GetAxis("LeftThrottle")  * movementSpeed;
+        float rInput = Input.GetAxis("RightThrottle") * movementSpeed;
+
+        lVelocity += lInput;
+        rVelocity += rInput;
+
+        if (lInput > 0 && rInput == 0)
+        {
+            rVelocity -= lInput/2;
+        }
+
+        if (rInput > 0 && lInput == 0)
+        {
+            lVelocity -= rInput/2;
+        }
+
+        lVelocity = Mathf.Clamp(lVelocity,-movementSpeed/2,movementSpeed);
+        rVelocity = Mathf.Clamp(rVelocity,-movementSpeed/2,movementSpeed);
+
+        if (lVelocity != 0) 
+        {
             Rigidbody.AddRelativeTorque(new Vector3(0, lVelocity,0));
             Rigidbody.AddRelativeForce(new Vector3(lVelocity, 0 ,0));
         }
 
-        if (rVelocity != 0) {
+        if (rVelocity != 0) 
+        {
             Rigidbody.AddRelativeTorque(new Vector3(0, -rVelocity,0));
             Rigidbody.AddRelativeForce(new Vector3(rVelocity, 0 ,0));
         }
